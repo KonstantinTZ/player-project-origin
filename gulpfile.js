90a5842c43bb6eb5cb078d824f1823f147674a06
Pred1995 <beleberdek@yandex.ru>
2024-03-15 11:50:23 +0300

    up

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 const gulp = require('gulp');
 
-// Tasks
 require('./gulp/dev.js');
 require('./gulp/docs.js');
 
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,44 @@
 const polygons = document.querySelectorAll('.polygon')
+const menu = document.querySelector('.menu')
+const drawer = document.querySelector('.drawer')
+const close = document.querySelector('.close')
 
-polygons.forEach(polygon => {
-    polygon.addEventListener('mouseover', (e) => {
-    })
+// polygons.forEach(polygon => {
+//     polygon.addEventListener('mouseover', (e) => {
+//         const top = e.target.closest('.column').querySelector('.column__top')
+//
+//         const color = getComputedStyle(top).backgroundColor
+//
+//         top.style.backgroundColor = color.replace('0.3)', '1)')
+//     })
+//
+//     polygon.addEventListener('mouseout', (e) => {
+//         const top = e.target.closest('.column').querySelector('.column__top')
+//
+//         const color = getComputedStyle(top).backgroundColor
+//
+//         console.log(color)
+//
+//         // top.style.backgroundColor = color.replace('1', '0.3')
+//     })
+// })
+
+menu.addEventListener('click', (e) => {
+    if (drawer.classList.contains('drawer__close')) {
+        drawer.classList.remove('drawer__close')
+        drawer.classList.add('drawer__open')
+    } else {
+        drawer.classList.add('drawer__close')
+        drawer.classList.remove('drawer__open')
+    }
+})
+
+close.addEventListener('click', (e) => {
+    if (drawer.classList.contains('drawer__close')) {
+        drawer.classList.remove('drawer__close')
+        drawer.classList.add('drawer__open')
+    } else {
+        drawer.classList.add('drawer__close')
+        drawer.classList.remove('drawer__open')
+    }
 })
