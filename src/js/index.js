bb93ba0fe302bb55ad23708d522060eb5c3d48ff
Pred1995 <beleberdek@yandex.ru>
2024-03-18 22:14:22 +0300

    up

--- src/js/index.js
+++ src/js/index.js
@@ -5,51 +5,16 @@ const close = document.querySelector('.close')
 
 polygons.forEach(polygon => {
 
-    const buttons = polygon.querySelectorAll('button')
-
-    buttons.forEach(button => {
-        button.addEventListener('mouseover', (e) => {
-            const top = e.target.closest('.column').querySelector('.column__top')
-
-            const color = getComputedStyle(top).backgroundColor
-
-            if (color === 'rgba(233, 117, 44, 0.7)') {
-                top.classList.remove('column__st3-active', 'column__st2-active')
-                top.classList.add('column__st2-2-active')
-            }
-
-            if (color === 'rgba(125, 97, 155, 0.7)') {
-                top.classList.remove('column__st3-active', 'column__st2-active')
-                top.classList.add('column__st3-2-active')
-            }
-
-        })
-
-        button.addEventListener('click', (e) => {
-            const top = e.target.closest('.column').querySelector('.column__top')
-
-            const color = getComputedStyle(top).backgroundColor
-
-            if (color.includes('233, 117, 44')) {
-                top.classList.remove('column__st3-active', 'column__st2-active', 'column__st2-2-active', 'column__st3-2-active')
-                top.classList.add('column__st2-3-active')
-            } else {
-                top.classList.remove('column__st3-active', 'column__st2-active', 'column__st2-2-active', 'column__st3-2-active')
-                top.classList.add('column__st3-3-active')
-            }
-        })
-    })
-
     polygon.addEventListener('mouseover', (e) => {
         const top = e.target.closest('.column').querySelector('.column__top')
 
         const color = getComputedStyle(top).backgroundColor
 
-        if (color === 'rgba(233, 117, 44, 0.7)') {
+        if (color === 'rgba(233, 117, 44, 0.3)') {
             top.classList.add('column__st2-active')
         }
 
-        if (color === 'rgba(125, 97, 155, 0.7)') {
+        if (color === 'rgba(125, 97, 155, 0.3)') {
             top.classList.add('column__st3-active')
         }
 
