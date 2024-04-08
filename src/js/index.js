52c789559a7c72a6760bc92c72696a7b74535cd3
Pred1995 <beleberdek@yandex.ru>
2024-04-08 22:07:24 +0300

    up

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,130 @@
+!(function( window, undefined ) {
+
+    var supportsOrientation = ( typeof window.orientation == 'number' && typeof window.onorientationchange == 'function' );
+
+    // Do nothing if we don't need to shim
+    if ( supportsOrientation ) return;
+
+    /* START: requestAnimationFrame shim
+     *
+     * By: Eric Möller
+     * URL: http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
+     */
+    (function() {
+        var lastTime = 0;
+        var vendors = ['ms', 'moz', 'webkit', 'o'];
+        for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+            window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
+            window.cancelRequestAnimationFrame = window[vendors[x] +
+            'CancelRequestAnimationFrame'];
+        }
+
+        if (!window.requestAnimationFrame)
+            window.requestAnimationFrame = function(callback, element) {
+                var currTime = new Date().getTime();
+                var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+                var id = window.setTimeout(function() {
+                        callback(currTime + timeToCall);
+                    },
+                    timeToCall);
+                lastTime = currTime + timeToCall;
+                return id;
+            };
+
+        if (!window.cancelAnimationFrame)
+            window.cancelAnimationFrame = function(id) {
+                clearTimeout(id);
+            };
+    } ());
+
+    /* END: requestAnimationFrame shim */
+
+    var cOrientationChange = function() {
+
+        this.currentOrientationAngle = -1;
+        this.lastOrientationAngle = -2;
+
+        this.currentOrientationState = this.lastOrientationState = ( window.innerWidth > window.innerHeight ) ? 'landscape': 'portrait';
+
+        var self = this;
+
+        // Setup deviceorientation listener (if it is available)
+        window.addEventListener( 'deviceorientation', function( e ) {
+            // Don't use unless we have a 3-axis implementation of device orientation
+            if ( e.alpha === null || e.alpha === undefined ) {
+                self.currentOrientationAngle = 0;
+                return;
+            }
+
+            // set orientation angle to nearest 90 degrees
+            self.currentOrientationAngle = Math.round( -e.gamma / 90) * 90;
+
+            // correction for holding the device upside down
+            if ( self.currentOrientationAngle == 0 && e.beta < 0 ) {
+                self.currentOrientationAngle = 180;
+            }
+
+        }, true );
+
+        this.manualOrientationChange = function() {
+
+            // landscape when width is biggest, otherwise portrait
+            self.currentOrientationState = ( window.innerWidth > window.innerHeight ) ? 'landscape': 'portrait';
+
+            if (  ( self.currentOrientationState !== self.lastOrientationState ) ||
+                ( self.lastOrientationAngle === -2 && self.currentOrientationAngle !== -1 ) ||
+                ( self.currentOrientationAngle !== 0 && self.currentOrientationAngle === -self.lastOrientationAngle )  ) {
+
+                if ( self.currentOrientationAngle === -1 ) self.currentOrientationAngle = 0;
+
+                // Update static window.orientation value
+                window.orientation = self.currentOrientationAngle;
+
+                // Create and dispatch pseudo-orientationchange event
+                var orientationEvent = window.document.createEvent( 'Event' );
+                orientationEvent.initEvent( 'orientationchange', true, true );
+                orientationEvent.orientation = this.currentOrientationAngle;
+                orientationEvent.mode = this.currentOrientationState;
+                window.dispatchEvent( orientationEvent );
+
+                // Fire event to window.onorientationchange assigned handler (if any)
+                if ( typeof window.onorientationchange == 'function' ) {
+                    window.onorientationchange.call( this, orientationEvent );
+                }
+
+                // Check for onorientationevent handler on body element and execute
+                var body = document.body;
+                if( body && body.getAttribute('onorientationchange')) {
+                    // Create new script and run function
+                    var onorientationchange_script = document.createElement('script');
+                    onorientationchange_script.type = "text/javascript";
+                    onorientationchange_script.textContent = "//<![CDATA[\n" + body.getAttribute('onorientationchange') + "\n//]]>";
+                    body.appendChild(onorientationchange_script);
+                    onorientationchange_script.parentNode.removeChild( onorientationchange_script );
+                }
+
+                self.lastOrientationAngle = self.currentOrientationAngle;
+                self.lastOrientationState = self.currentOrientationState;
+
+            }
+
+            window.requestAnimationFrame( self.manualOrientationChange.bind( self ) );
+
+        };
+
+        this.manualOrientationChange();
+
+    };
+
+    //*** WINDOW EVENTLISTENER SHIM
+    var orientationHandler = new cOrientationChange();
+
+    window.orientation = orientationHandler.currentOrientationAngle === -1 ? 0: orientationHandler.currentOrientationAngle;
+
+    window.onorientationchange = function( e ) {};
+
+})( window );
+
 
 const polygons = document.querySelectorAll('.polygon')
 const menu = document.querySelector('.menu')
@@ -66,11 +193,9 @@ const isDesktop = () => {
 
 let cont = document.querySelector(".container");
 
-// window.addEventListener("resize", () => {
-//     if (isDesktop()) {
-//         scale()
-//     }
-// });
+window.addEventListener('orientationchange', function(e) {
+    scale()
+}, true);
 
 function scale() {
     let width = document.body.clientWidth;
@@ -147,12 +272,9 @@ dialogClose.addEventListener("click", () => {
     dialog.style.visibility = 'hidden';
 });
 
-
-window.addEventListener("resize", function() {
-    scale()
-
-}, false);
-
+if (screen.orientation) { // Property doesn't exist on screen in IE11
+    screen.orientation.addEventListener("change", scale);
+};
 
 
 function handlePortraitOrLandscape() {
